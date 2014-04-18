70520cc515f11773d669ea7485e225aa25bf7293
Thomas Ackermann <th.acker@arcor.de>
2014-04-18 17:39:53 +0200

    add a simple web interface for pihome data
    a standard lighttpd installation is assumed for
    this functionality; html.py must be run as root

---
+++ html.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+#
+# Copyright 2014 Thomas Ackermann
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Simple web interface for pihome data. 
+# A standard lighttpd installation is assumed for this functionality
+# and this script must be run as root.
+#
+
+import glob, string, time
+
+path = "/tmp/pihome/*"
+html = "/var/www/index.html"
+
+while True:
+    hd = open(html, "w")
+    hd.write('<html><head><title>\n')
+    hd.write('pihome data\n')
+    hd.write('</title><meta http-equiv="refresh" content="20">\n')
+    hd.write('<head><body>\n')
+    hd.write('<h2>pihome data</h2><table border="1">\n')
+    files = glob.glob(path)
+    for file in files:
+        fd = open(file)
+        for line in fd:
+            tok = string.strip(line).split()
+            hd.write('<tr><td align="right">' + tok[0] + '</td>\n')
+            hd.write('<td align="left">' + string.join(tok[1:]) + '</td></tr>\n')
+        fd.close()
+    hd.write('</table></body></html>\n')
+    hd.close()
+    time.sleep(20)
+    
