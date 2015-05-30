77bc0dc1748683494157a8b5031161b4d019b754
Thomas Ackermann <th.acker@arcor.de>
2015-05-30 16:45:12 +0200

    upgrade to bluez 5.30

--- Test/ble_discover.py
+++ Test/ble_discover.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Copyright 2014 Thomas Ackermann
+# Copyright 2014-2015 Thomas Ackermann
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -80,64 +80,67 @@ def read_conprop(handle):
   return ret_str
 
 adr = sys.argv[1]
-tool = pexpect.spawn('gatttool518 -b ' + adr + ' --interactive')
+tool = pexpect.spawn('gatttool530 -b ' + adr + ' --interactive')
 tool.expect('\[LE\]>')
 
-sys.stdout.write("Trying to connect to '" + 
-  adr + "'. (Device must be advertising ...)\n")
+sys.stdout.write("Trying to connect to '" + adr + "' (device must be advertising) ...\n")
 tool.sendline('connect')
 tool.expect('success')
 
-sys.stdout.write("Starting discovery ...\n")
+sys.stdout.write("Starting discovery (this may take some time) ...\n")
 
+# char-desc is running asynchronously and no kind of 'finished'
+# message is printed by gatttool, so we have to greedily read
+# everything until pexpect timeouts
 tool.sendline('char-desc')
-tool.expect('finished')
+tool.expect(['WILLNOTMATCH', pexpect.TIMEOUT], timeout=30)
+lines = tool.before.split('\r')
 
-lines = tool.before.split("\r\n")[2:-1]
+sys.stdout.write("HANDLE : VALUE\n")
+sys.stdout.write("--------------\n")
 
 for line in lines:
-  tok = line
-  tok = re.sub("^.*handle: ", "", tok)
-  tok = re.sub("uuid:", "", tok)
-  tok = re.sub(",", "", tok)
-  (handle, uuid) = tok.split()
-  handle = handle.lower()
-  uuid = uuid.lower()
-  if uuid == "2800":
-    sys.stdout.write(handle + " : primary --------------------------------------------" + "\n")
-  elif len(uuid) > 4:
-    sys.stdout.write(handle + " :        characteristic : '" + uuid + "'" + "\n")
-  elif uuid == "2803":
-    sys.stdout.write(handle + " :              property : ")
-    sys.stdout.write(read_property(handle) + "\n")
-  elif uuid == "2901":
-    sys.stdout.write(handle + " :           description : ")
-    sys.stdout.write(read_string(handle) + "\n")
-  elif uuid == "2a00":
-    sys.stdout.write(handle + " :                  name : ")
-    sys.stdout.write(read_string(handle) + "\n")
-  elif uuid == "2a04":
-    sys.stdout.write(handle + " :  preferred connection : ")
-    sys.stdout.write(read_conprop(handle) + "\n")
-  elif uuid == "2a24":
-    sys.stdout.write(handle + " :          model number : ")
-    sys.stdout.write(read_string(handle) + "\n")
-  elif uuid == "2a25":
-    sys.stdout.write(handle + " :         serial number : ")
-    sys.stdout.write(read_string(handle) + "\n")
-  elif uuid == "2a26":
-    sys.stdout.write(handle + " :     firmware revision : ")
-    sys.stdout.write(read_string(handle) + "\n")
-  elif uuid == "2a27":
-    sys.stdout.write(handle + " :     hardware revision : ")
-    sys.stdout.write(read_string(handle) + "\n")
-  elif uuid == "2a28":
-    sys.stdout.write(handle + " :     software revision : ")
-    sys.stdout.write(read_string(handle) + "\n")
-  elif uuid == "2a29":
-    sys.stdout.write(handle + " :          manufacturer : ")
-    sys.stdout.write(read_string(handle) + "\n")
-
-sys.stdout.write("Discovery finished.\n")
+  if "handle:" in line:
+    tok = line
+    tok = re.sub("^.*handle: ", "", tok)
+    tok = re.sub("uuid:", "", tok)
+    tok = re.sub(",", "", tok)
+    (handle, uuid) = tok.split()
+    handle = handle.lower()
+    uuid = uuid.lower()
+    if "2800-" in uuid:
+      sys.stdout.write(handle + " : primary ------------------------------------------------------\n")
+    elif "2803-" in uuid:
+      sys.stdout.write(handle + " :              property : ")
+      sys.stdout.write(read_property(handle) + "\n")
+    elif "2901-" in uuid:
+      sys.stdout.write(handle + " :           description : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    elif "2a00-" in uuid:
+      sys.stdout.write(handle + " :                  name : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    elif "2a04-" in uuid:
+      sys.stdout.write(handle + " :  preferred connection : ")
+      sys.stdout.write(read_conprop(handle) + "\n")
+    elif "2a24-" in uuid:
+      sys.stdout.write(handle + " :          model number : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    elif "2a25-" in uuid:
+      sys.stdout.write(handle + " :         serial number : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    elif "2a26-" in uuid:
+      sys.stdout.write(handle + " :     firmware revision : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    elif "2a27-" in uuid:
+      sys.stdout.write(handle + " :     hardware revision : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    elif "2a28-" in uuid:
+      sys.stdout.write(handle + " :     software revision : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    elif "2a29-" in uuid:
+      sys.stdout.write(handle + " :          manufacturer : ")
+      sys.stdout.write(read_string(handle) + "\n")
+    else:
+      sys.stdout.write(handle + " :        characteristic : '" + uuid + "'" + "\n")
 
 
