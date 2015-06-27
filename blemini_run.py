ca221cff05d679034fee6a3fb03bfbe1a47ff170
Thomas Ackermann <th.acker@arcor.de>
2015-06-27 16:31:05 +0200

    first version of real startup script

---
+++ blemini_run.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+#
+# Copyright 2013-2015 Thomas Ackermann
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Read digital inputs from a RedBearLabs BLEmini. 
+# BLEmini is a BLE (Bluetooth low energy) device so by
+# automating gatttool (from BlueZ 5.30) with
+# pexpect (3.1) we are able to read and write values.
+#
+# Usage: blemini_test.py BLUETOOTH_ADR
+#
+# To find the address of your BLEmini run 'sudo hcitool lescan'
+# To power up your bluetooth dongle run 'sudo hciconfig hci0 up'
+#
+
+#
+# BLEMini Biscuit v2.0 handles
+# (discovered by 'primary' resp. 'characteristic' cmd in gatttool):
+#
+# 0x12: read list of bytes transferred to BLEMini via serial connection
+# 0x16: write byte to BLEMini to be received via serial connection
+# 0x19: set baudrate for serial port (value is remanent; possible values are: 
+#                      00=9600, 01=19200, 02=38400, 03=57600(default), 04=115200)
+#
+
+import os
+import sys
+import time
+import random
+import pexpect
+from datetime import datetime
+
+adr = sys.argv[1]
+
+logdir = "/tmp/pihome"
+try:
+  os.mkdir(logdir)
+except:
+  pass
+
+cnt = 0
+rnd = 0
+val = 0
+err = 0
+exc = 0
+act = 0
+stamp = ""
+handle = ""
+
+def log_values():
+  print adr, " CNT %04d, RND 0x%02X, DATA 0x%02X, ERR %d, EXC %d, ACT %d" % (cnt, rnd, val, err, exc, act)
+  print adr, " STAMP '%s'" % stamp
+
+  data = open(logdir+"/"+adr, "w")
+  data.write("  RND 0x%02X\n" % rnd)
+  data.write(" DATA 0x%02X\n" % val)
+  data.write("  ERR %d\n" % err)
+  data.write("EXCPT %d\n" % exc)
+  data.write("ACTEX %d\n" % act)
+  data.write("STAMP '%s'\n" % stamp)
+  data.close()
+
+while True:
+
+  try:
+
+    print adr, " Trying to connect ..."
+
+    tool = pexpect.spawn('gatttool530 -b ' + adr + ' --interactive')
+    tool.expect('\[LE\]>')
+    tool.sendline('connect')
+    tool.expect('success')
+
+    # find handle
+    cons = pexpect.run('hcitool con')
+    cons = cons.split("\r\n")
+    for con in cons:
+      if adr in con:
+        tok = con.split()
+        handle = tok[4]
+
+    # set baudrate to 9600
+    tool.sendline('char-write-cmd 0x19 00')
+    tool.expect('\[LE\]>')
+
+    while True:
+
+        rnd = random.randint(0, 255)
+
+        # send byte to BLEMini
+        tool.sendline('char-write-cmd 0x16 ' + ("%02X" % rnd))
+        tool.expect('\[LE\]>')
+        
+        # read byte/s from BLEMini
+        tool.sendline('char-read-hnd 0x12')
+        tool.expect('descriptor: .*? \r') 
+        v = tool.after.split()
+        val = int(float.fromhex(v[1]))
+
+        cnt = cnt + 1
+
+        if val != rnd:
+            err = err + 1
+
+        stamp = datetime.now().ctime()
+        act = 0
+
+        log_values()
+
+        time.sleep(3)
+
+  except KeyboardInterrupt:
+    tool.sendline('quit')
+    tool.close()
+    sys.exit()
+
+  except:
+    if handle != "":
+        pexpect.run('sudo hcitool ledc ' + handle)
+    tool.sendline('quit')
+    tool.close(force=True)
+    exc = exc + 1
+    act = 1
+    log_values()
+
--- sensortag_run.py
+++ sensortag_run.py
@@ -2,7 +2,7 @@
 
 #
 # Copyright 2013 Michael Saunby
-# Copyright 2013-2014 Thomas Ackermann
+# Copyright 2013-2015 Thomas Ackermann
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
 #
 # Read sensors from the TI SensorTag. It's a
 # BLE (Bluetooth low energy) device so by
-# automating gatttool (from BlueZ 5.14) with
+# automating gatttool (from BlueZ 5.30) with
 # pexpect (3.1) we are able to read and write values.
 #
 # Usage: sensortag_test.py BLUETOOTH_ADR
@@ -29,96 +29,185 @@
 # To power up your bluetooth dongle run 'sudo hciconfig hci0 up'
 #
 
+#
+# SensorTag v1.5 handle ranges 
+# (discovered by 'primary' resp. 'characteristic' cmd in gatttool):
+#
+#   Temperature: 0x23 - 0x2d (set 0x29 = 01; read 0x25)
+# Accelerometer: 0x2e - 0x38
+#      Humidity: 0x39 - 0x43 (set 0x3f = 01; read 0x3b)
+#  Magnetometer: 0x44 - 0x4e
+#     Barometer: 0x4f - 0x5d (set 0x55 = 01/02; read 0x51/0x5b)
+#     Gyroscope: 0x5e - 0x68
+#          Keys: 0x69 - 0x6d
+#          Test: 0x6e - 0x74 (POST = 0x70; bits: 0 0 gyro press acc mag hum temp, 
+#                                                0x3f means "OK"
+#
+
+import os
 import sys
 import time
 import pexpect
+from datetime import datetime
+
 from sensortag_funcs import *
 
-# start gatttool
 adr = sys.argv[1]
-tool = pexpect.spawn('gatttool514 -b ' + adr + ' --interactive')
-tool.expect('\[LE\]>')
 
-# bug in pexpect? automating gatttool works only if we are using a logfile!
-# TODO: check again with pexpect 3.1 and gatttool 5.14
-logfile = open("/dev/null", "w")
-tool.logfile = logfile
-
-# connect to SensorTag
-print adr, " Trying to connect. You might need to press the side button ..."
-tool.sendline('connect')
-tool.expect('\[LE\]>')
-
-print adr, " Enabling sensors ..."
-# enable temperature sensor
-tool.sendline('char-write-cmd 0x29 01')
-tool.expect('\[LE\]>')
-
-# enable humidity sensor
-tool.sendline('char-write-cmd 0x3c 01')
-tool.expect('\[LE\]>')
-
-# enable barometric pressure sensor
-tool.sendline('char-write-cmd 0x4f 02')
-tool.expect('\[LE\]>')
-
-tool.sendline('char-read-hnd 0x52')
-tool.expect('descriptor: .*? \r')
-
-after = tool.after
-v = after.split()[1:] 
-vals = [long(float.fromhex(n)) for n in v]
-barometer = Barometer( vals )
-tool.sendline('char-write-cmd 0x4f 01')
-tool.expect('\[LE\]>')
-
-# wait for the sensors to become ready
-time.sleep(1)
+logdir = "/tmp/pihome"
+try:
+  os.mkdir(logdir)
+except:
+  pass
 
 cnt = 0
+it = 0
+at = 0
+ht = 0
+pt = 0
+hu = 0
+pr = 0
+exc = 0
+act = 0
+post = ""
+stamp = ""
+handle = ""
+
+def log_values():
+  print adr, "   CNT %d" % cnt
+  print adr, "  POST 0x%s" % post
+  print adr, " IRTMP %.1f" % it
+  print adr, " AMTMP %.1f" % at
+  print adr, " HMTMP %.1f" % ht
+  print adr, " BRTMP %.1f" % pt
+  print adr, " HUMID %.0f" % hu
+  print adr, " BAROM %.0f" % pr
+  print adr, " EXCPT %d" % exc
+  print adr, " ACTEX %d" % act
+  print adr, " STAMP '%s'" % stamp
+
+  data = open(logdir+"/"+adr, "w")
+  data.write(" POST 0x%s\n" % post)
+  data.write("IRTMP %.1f\n" % it)
+  data.write("AMTMP %.1f\n" % at)
+  data.write("HMTMP %.1f\n" % ht)
+  data.write("BRTMP %.1f\n" % pt)
+  data.write("HUMID %.0f\n" % hu)
+  data.write("BAROM %.0f\n" % pr)
+  data.write("EXCPT %d\n" % exc)
+  data.write("ACTEX %d\n" % act)
+  data.write("STAMP '%s'\n" % stamp)
+  data.close()
+
 while True:
 
-    cnt = cnt + 1
-    print adr, " CNT %d" % cnt
+  try:
 
-    # read temperature sensor
-    tool.sendline('char-read-hnd 0x25')
-    tool.expect('descriptor: .*? \r') 
-    v = tool.after.split()
-    rawObjT = long(float.fromhex(v[2] + v[1]))
-    rawAmbT = long(float.fromhex(v[4] + v[3]))
-    (at, it) = calcTmp(rawAmbT, rawObjT)
+    print adr, " Trying to connect. You might need to press the side button ..."
 
-    # read humidity sensor
-    tool.sendline('char-read-hnd 0x38')
-    tool.expect('descriptor: .*? \r') 
-    v = tool.after.split()
-    rawT = long(float.fromhex(v[2] + v[1]))
-    rawH = long(float.fromhex(v[4] + v[3]))
-    (ht, h) = calcHum(rawT, rawH)
+    tool = pexpect.spawn('gatttool530 -b ' + adr + ' --interactive')
+    tool.expect('\[LE\]>')
+    tool.sendline('connect')
+    tool.expect('success')
 
-    # read barometric pressure sensor
-    tool.sendline('char-read-hnd 0x4B')
-    tool.expect('descriptor: .*? \r') 
-    v = tool.after.split()
-    rawT = long(float.fromhex(v[2] + v[1]))
-    rawP = long(float.fromhex(v[4] + v[3]))
-    (pt, p) = barometer.calc(rawT, rawP)
+    print adr, " Switching to a lower energy connection ..."
 
-    print adr, " IRTMP %.1f" % it
-    print adr, " AMTMP %.1f" % at
-    print adr, " HMTMP %.1f" % ht
-    print adr, " BRTMP %.1f" % pt
-    print adr, " HUMID %.0f" % h
-    print adr, " BAROM %.0f" % p
+    # gatttool not really connects with enough 'low energy' so reconfigure
+    # the connection to the values preferred by SensorTag (see characteristic 0x2A04):
+    # i.e. min interval to 100ms and max interval to 200ms.
+    # By this the current needed for 'just being connected' in the SensorTag 
+    # drops from 0.35mA to 0.01mA.
+    cons = pexpect.run('hcitool con')
+    cons = cons.split("\r\n")
+    for con in cons:
+      if adr in con:
+        tok = con.split()
+        handle = tok[4]
+        state = tok[6]
+        error = pexpect.run('sudo hcitool lecup --handle ' + handle + ' --min 80 --max 160')
+        if error <> "":
+          print "hcittool error: '" + error + "' (handle: " + handle + ", state: " + state + ")"
 
-    data = open("/home/pi/tmp/pihome/"+adr, "w")
-    data.write("IRTMP %.1f\n" % it)
-    data.write("AMTMP %.1f\n" % at)
-    data.write("HMTMP %.1f\n" % ht)
-    data.write("BRTMP %.1f\n" % pt)
-    data.write("HUMID %.0f\n" % h)
-    data.write("BAROM %.0f\n" % p)
-    data.close()
+    print adr, " Enabling sensors ..."
 
-    time.sleep(10)
+    # enable temperature sensor
+    tool.sendline('char-write-req 0x29 01')
+    tool.expect('\[LE\]>')
+
+    # enable humidity sensor
+    tool.sendline('char-write-req 0x3f 01')
+    tool.expect('\[LE\]>')
+
+    # enable barometric pressure sensor
+    tool.sendline('char-write-req 0x55 02')
+    tool.expect('\[LE\]>')
+
+    tool.sendline('char-read-hnd 0x5b')
+    tool.expect('descriptor: .*? \r')
+
+    after = tool.after
+    v = after.split()[1:] 
+    vals = [long(float.fromhex(n)) for n in v]
+    barometer = Barometer( vals )
+
+    tool.sendline('char-write-req 0x55 01')
+    tool.expect('\[LE\]>')
+
+    # wait for the sensors to become ready
+    time.sleep(1)
+
+    while True:
+
+        # read POST result
+        tool.sendline('char-read-hnd 0x70')
+        tool.expect('descriptor: .*? \r') 
+        v = tool.after.split()
+        post = v[1]
+
+        # read temperature sensor
+        tool.sendline('char-read-hnd 0x25')
+        tool.expect('descriptor: .*? \r') 
+        v = tool.after.split()
+        rawObjT = long(float.fromhex(v[2] + v[1]))
+        rawAmbT = long(float.fromhex(v[4] + v[3]))
+        (at, it) = calcTmp(rawAmbT, rawObjT)
+
+        # read humidity sensor
+        tool.sendline('char-read-hnd 0x3b')
+        tool.expect('descriptor: .*? \r') 
+        v = tool.after.split()
+        rawT = long(float.fromhex(v[2] + v[1]))
+        rawH = long(float.fromhex(v[4] + v[3]))
+        (ht, hu) = calcHum(rawT, rawH)
+
+        # read barometric pressure sensor
+        tool.sendline('char-read-hnd 0x51')
+        tool.expect('descriptor: .*? \r') 
+        v = tool.after.split()
+        rawT = long(float.fromhex(v[2] + v[1]))
+        rawP = long(float.fromhex(v[4] + v[3]))
+        (pt, pr) = barometer.calc(rawT, rawP)
+
+        cnt = cnt + 1
+
+        stamp = datetime.now().ctime()
+        act = 0
+
+        log_values()
+
+        time.sleep(3)
+
+  except KeyboardInterrupt:
+    tool.sendline('quit')
+    tool.close()
+    sys.exit()
+
+  except:
+    if handle != "":
+        pexpect.run('sudo hcitool ledc ' + handle)
+    tool.sendline('quit')
+    tool.close(force=True)
+    exc = exc + 1
+    act = 1
+    log_values()
+
