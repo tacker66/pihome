975bf4405594ecb156a8f80fd9fa56f28a083c6d
Thomas Ackermann <th.acker@arcor.de>
2023-08-04 13:22:17 +0000

    Add a WebServer

--- MicroPython/beaconscanner.py
+++ MicroPython/beaconscanner.py
@@ -5,6 +5,7 @@ import aioble
 import binascii
 import thermobeacon
 import Pico_LCD_114_V2
+import webserver
 
 config = dict()
 def read_config(file):
@@ -14,14 +15,19 @@ def read_config(file):
         if len(line) > 0 and line[0] != "#":
             tok = line.split("=")
             config[tok[0].strip()] = tok[1].strip()
+read_config('beacons.conf')
+
+write_lock = asyncio.Lock()
+
+SCANTIME  = 5000
+SLEEPTIME = 5000
+DISPTIME  = 5000
 
 devices = dict()
-async def scan():
-    async with aioble.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner:
+async def scan_devices():
+    async with aioble.scan(SCANTIME, interval_us=30000, window_us=30000, active=True) as scanner:
         async for result in scanner:
-            
             device = result.device.addr_hex()
-
             if device not in devices:
                 device_data = dict()
                 device_data["manufacturer_id"] = 0
@@ -29,44 +35,64 @@ async def scan():
                 devices[device] = device_data
             else:
                 device_data = devices[device]
-                
             device_data["name"] = result.name()
             device_data["rssi"] = result.rssi
             device_data["connectable"] = result.connectable
-
             for data in result.manufacturer():
                 device_data["manufacturer_id"] = data[0]
                 device_data["manufacturer"].add(data[1])
-
-async def main():
-    await scan()
-
+        
 values = dict()
-read_config('beacons.conf')
+async def calc_values():
+    for device in devices:
+        #print(devices[device]["name"], devices[device]["rssi"], devices[device]["connectable"], device)
+        mid = devices[device]["manufacturer_id"]
+        for data in sorted(devices[device]["manufacturer"]):
+            #print(mid, str(binascii.hexlify(data, ' '), "utf-8"))
+            if device in config:
+                if device not in values:
+                    values[device] = dict()
+                    values[device]["ERR"] = 0
+                    values[device]["ACT"] = 0
+                if thermobeacon.can_decode(mid, data):
+                    tmp, hum, bat = thermobeacon.decode(mid, data)
+                    values[device]["TMP"] = tmp
+                    values[device]["HUM"] = hum
+                    values[device]["BAT"] = bat
+
+async def get_values():
+    while True:
+        await write_lock.acquire()
+        await scan_devices()
+        await calc_values()
+        write_lock.release()
+        await asyncio.sleep_ms(SLEEPTIME)
 
 disp = Pico_LCD_114_V2.LCD_114()
+async def display_values():
+    while True:
+        while write_lock.locked():
+            await asyncio.sleep(1)
+        for device in values:
+            name = config[device]
+            print(device, name, values[device])
+            pos = int(config[name+".POS"])
+            act = int(values[device]["ACT"])
+            msg = "{:.1f} {:.1f} {:.1f} ({:d})".format(values[device]["TMP"], values[device]["HUM"], values[device]["BAT"], values[device]["ERR"])
+            disp.display(pos, name, msg, False, act)
+        await asyncio.sleep_ms(DISPTIME)
+        webserver.update_index(config, values)
+        await asyncio.sleep_ms(DISPTIME)
+
+async def start_webserver():
+    webserver.wlan_connect(config["SSID"], config["PASS"])
+    webserver.start_webserver("BeaconScanner")
+    
+async def main():
+    await asyncio.gather(
+        get_values(),
+        display_values(),
+        start_webserver()
+        )
 
 asyncio.run(main())
-    
-for device in devices:
-    #print(devices[device]["name"], devices[device]["rssi"], devices[device]["connectable"], device)
-    mid = devices[device]["manufacturer_id"]
-    for data in sorted(devices[device]["manufacturer"]):
-        #print(str(binascii.hexlify(data, ' '), "utf-8"))
-        if device in config:
-            if device not in values:
-                values[device] = dict()
-                values[device]["ERR"] = 0
-                values[device]["ACT"] = 0
-            if thermobeacon.can_decode(mid, data):
-                tmp, hum, bat = thermobeacon.decode(mid, data)
-                values[device]["TMP"] = tmp
-                values[device]["HUM"] = hum
-                values[device]["BAT"] = bat
-
-for device in values:
-    name = config[device]
-    pos = int(config[name+".POS"])
-    act = int(values[device]["ACT"])
-    msg = "{:.1f} {:.1f} {:.1f} ({:d})".format(values[device]["TMP"], values[device]["HUM"], values[device]["BAT"], values[device]["ERR"])
-    disp.display(pos, name, msg, False, act)
---
+++ MicroPython/webserver.py
@@ -0,0 +1,89 @@
+
+# https://github.com/tacker66/picoweb/tree/micropython
+
+# mip.install("pkg_resources")
+
+from machine import Pin
+import micropython
+import utime
+import network
+import picoweb
+
+acp_if = network.WLAN(network.AP_IF)
+sta_if = network.WLAN(network.STA_IF)
+
+acp_if.active(False)
+sta_if.active(True)
+
+def _wlan_status(status):
+    if status == network.STAT_IDLE:
+        return 'STAT_IDLE'
+    elif status == network.STAT_CONNECTING:
+        return 'STAT_CONNECTING'
+    elif status == network.STAT_WRONG_PASSWORD:
+        return 'STAT_WRONG_PASSWORD'
+    elif status == network.STAT_NO_AP_FOUND:
+        return 'STAT_NO_AP_FOUND'
+    elif status == network.STAT_GOT_IP:
+        return 'STAT_GOT_IP'
+    else:
+        return "unknown WLAN status: {}".format(status) 
+
+def wlan_status():
+    return _wlan_status(sta_if.status())
+
+def wlan_connect(ssid, pwd):
+    try:
+        if sta_if.isconnected():
+            print('disconnecting')
+            sta_if.disconnect()
+            while sta_if.isconnected():
+                print(wlan_status())
+                utime.sleep_ms(1000)
+        if not sta_if.isconnected():
+            print('connecting to', ssid)
+            sta_if.connect(ssid, pwd)
+            while not sta_if.isconnected():
+                toggle_led()
+                print(wlan_status())
+                utime.sleep_ms(1000)
+    except:
+        pass
+    print(wlan_status(), sta_if.ifconfig())
+
+index_html = """
+<html>
+  <head>
+    <title>Hello</title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+  </body>
+</html>
+"""
+
+def update_index(config, values):
+    global index_html
+    s = """<html>
+                <head>
+                    <title>pihome data</title><meta http-equiv="refresh" content="30">
+                        <head>
+                            <body>
+                                <h2>pihome data</h2>"""                       
+    for device in values:
+        name = config[device]
+        s = s + '\n<h3>' + device + ' (' + name + ')' + '</h3>' + '<table border="1">\n'
+        for value in sorted(values[device]):
+            s = s + '<tr><td align="right">' + str(value) + '</td>\n' + '<td align="left">' + str(values[device][value]) + '</td></tr>\n'
+        s = s + '</table>\n'
+    s = s + '</body></html>\n'
+    index_html = s
+    
+def indexhtml(req, resp):
+    yield from resp.awrite(index_html)
+
+ROUTES  = [("/", indexhtml), ]
+
+def start_webserver(name):
+    app = picoweb.WebApp(name, ROUTES)
+    app.run(debug=0, port=80, host="0.0.0.0")
