ef181cc8f951731aa911fd72996e1f6c2f2e76a2
Thomas Ackermann <th.acker@arcor.de>
2023-08-12 18:24:42 +0200

    Minimal and clean startup

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,6 @@
 
-print("=== main.py ===")
-
-import micropython
-micropython.opt_level(3)
+import gc
+gc.collect()
+#print(gc.mem_free())
 
 exec(open("beaconscanner.py").read(), globals())
-#exec(open("rpi_boot.py").read(), globals())
-
-print("=== finished ===")
