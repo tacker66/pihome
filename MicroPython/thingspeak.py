98677fc172d21c8eb049f628131bf80ce9eeff54
Thomas Ackermann <th.acker@arcor.de>
2023-08-12 19:08:36 +0200

    Save some more bytes

--- MicroPython/thingspeak.py
+++ MicroPython/thingspeak.py
@@ -9,19 +9,19 @@ import gc
 WAITTIME = 20 # thingspeak only allows ~8.200 messages per day; messages cannot be sent faster than every 10.6 seconds
 
 url = ""
-telegram_list = list()
 last_time = 0
+telegram_list = list()
 
 def update(config, values):
     global telegram_list, url
     if url == "":
-        url = config["URL"] +  "?"
+        url = config["URL"] + "?"
     for device in values:
         send_values = dict()
         name  = config[device]
         symbs = config["SYMB"].split()
         key   = config[config[name + ".KEY"]]
-        send_values["api_key"] = key
+        send_values["key"] = key
         for symb in symbs:
             value = values[device][symb]
             field_name = "field" + config[name + "." + symb]
@@ -36,11 +36,12 @@ def send():
     cur_time = time.time()
     if len(telegram_list) > 0 and (cur_time - last_time) > WAITTIME:
         telegram = url + telegram_list.pop(0)
-        print(len(telegram_list), "SEND", telegram)
         try:
             r = requests.post(telegram)
             log = str(r.status_code) + " " + r.text
         except:
-            log = "Exception"
-        print(log)
+            log = "Err"
+        if log == "Err" or r.text == "0": # lazy evaluation ?
+            print(len(telegram_list), "send", telegram)
+            print(log)
         last_time = cur_time
--- MicroPython/webserver.py
+++ MicroPython/webserver.py
@@ -1,8 +1,6 @@
 
 # https://github.com/tacker66/picoweb/tree/micropython
 
-from machine import Pin
-import micropython
 import utime
 import network
 import picoweb
@@ -15,17 +13,15 @@ sta_if.active(True)
 
 def _wlan_status(status):
     if status == network.STAT_IDLE:
-        return 'STAT_IDLE'
+        return 'IDLE'
     elif status == network.STAT_CONNECTING:
-        return 'STAT_CONNECTING'
-    elif status == network.STAT_WRONG_PASSWORD:
-        return 'STAT_WRONG_PASSWORD'
+        return 'CONNECTING'
     elif status == network.STAT_NO_AP_FOUND:
-        return 'STAT_NO_AP_FOUND'
+        return 'NO_AP_FOUND'
     elif status == network.STAT_GOT_IP:
         return 'STAT_GOT_IP'
     else:
-        return "unknown WLAN status: {}".format(status) 
+        return "UNKNOWN: {}".format(status) 
 
 def wlan_status():
     return _wlan_status(sta_if.status())
@@ -33,13 +29,11 @@ def wlan_status():
 def wlan_connect(ssid, pwd):
     try:
         if sta_if.isconnected():
-            print('disconnecting')
             sta_if.disconnect()
             while sta_if.isconnected():
                 print(wlan_status())
                 utime.sleep_ms(1000)
         if not sta_if.isconnected():
-            print('connecting to', ssid)
             sta_if.connect(ssid, pwd)
             while not sta_if.isconnected():
                 print(wlan_status())
@@ -48,25 +42,11 @@ def wlan_connect(ssid, pwd):
         pass
     print(wlan_status(), sta_if.ifconfig())
 
-index_html = """
-<html>
-  <head>
-    <title>BeaconScanner</title>
-  </head>
-  <body>
-    <h1>BeaconScanner</h1>
-  </body>
-</html>
-"""
+index_html = '<html><head><title>Hello</title></head><body><h1>Hello</h1></body></html>'
 
 def update(config, values):
     global index_html
-    s = """<html>
-                <head>
-                    <title>pihome data</title><meta http-equiv="refresh" content="30">
-                        <head>
-                            <body>
-                                <h2>pihome data</h2>"""                       
+    s = '<html><head><title>pihome</title><meta http-equiv="refresh" content="30"><head><body><h2>pihome</h2>'                       
     for device in values:
         name = config[device]
         s = s + '\n<h3>' + device + ' (' + name + ')' + '</h3>' + '<table border="1">\n'
