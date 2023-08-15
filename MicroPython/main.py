5f9850ec29c23639c23b80d8824c433aa0f57078
Thomas Ackermann <th.acker@arcor.de>
2023-08-15 18:19:06 +0200

    Start collecting startup statistics

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,6 +1,15 @@
 
-import gc
-gc.collect()
-#print(gc.mem_free())
+test = 1
 
-exec(open("beaconscanner.py").read(), globals())
+if test:
+    # 173344 v1.20.0-348-g24a6e951e on 2023-08-10
+    import gc
+    gc.collect()
+    print("MEM", gc.mem_free())
+
+    # 753664 v1.20.0-348-g24a6e951e on 2023-08-10
+    import os
+    s = os.statvfs('//')
+    print("FLASH", s[0]*s[3])
+else:
+    exec(open("beaconscanner.py").read(), globals())
